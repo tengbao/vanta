c213e23429ddf2d4032633d04cda2e1d75985b87
Teng Bao <teng@strikingly.com>
2019-08-20 09:41:55 +0800

    small cleanup

--- src/vanta.net.js
+++ src/vanta.net.js
@@ -1,5 +1,5 @@
 import VantaBase, {VANTA} from './_base.js'
-import {rn, ri, sample, mobileCheck} from './helpers.js'
+import {rn, ri, mobileCheck} from './helpers.js'
 
 THREE.Color.prototype.getBrightness = function() {
   // https://www.w3.org/TR/AERT#color-contrast
@@ -64,57 +64,57 @@ class Effect extends VantaBase {
   }
 
   onInit() {
-    this.cont = new THREE.Group();
-    this.cont.position.set(0,0,0);
-    this.scene.add(this.cont);
+    this.cont = new THREE.Group()
+    this.cont.position.set(0,0,0)
+    this.scene.add(this.cont)
 
-    let n = this.options.points;
-    let { spacing } = this.options;
+    let n = this.options.points
+    let { spacing } = this.options
     if (mobileCheck()) {
-      n = ~~(n * 0.75);
-      spacing = ~~(spacing * 0.65);
+      n = ~~(n * 0.75)
+      spacing = ~~(spacing * 0.65)
     }
 
-    const numPoints = n * n * 2;
-    this.linePositions = new Float32Array( numPoints * numPoints * 3 );
-    this.lineColors = new Float32Array( numPoints * numPoints * 3 );
+    const numPoints = n * n * 2
+    this.linePositions = new Float32Array( numPoints * numPoints * 3 )
+    this.lineColors = new Float32Array( numPoints * numPoints * 3 )
 
     const colorB = (new THREE.Color(this.options.color)).getBrightness()
     const bgB = (new THREE.Color(this.options.backgroundColor)).getBrightness()
-    this.blending =  colorB > bgB ? 'additive' : 'subtractive';
+    this.blending =  colorB > bgB ? 'additive' : 'subtractive'
 
-    const geometry = new THREE.BufferGeometry();
-    geometry.addAttribute('position', new THREE.BufferAttribute(this.linePositions, 3).setDynamic(true));
-    geometry.addAttribute('color', new THREE.BufferAttribute(this.lineColors, 3).setDynamic(true));
-    geometry.computeBoundingSphere();
-    geometry.setDrawRange( 0, 0 );
+    const geometry = new THREE.BufferGeometry()
+    geometry.addAttribute('position', new THREE.BufferAttribute(this.linePositions, 3).setDynamic(true))
+    geometry.addAttribute('color', new THREE.BufferAttribute(this.lineColors, 3).setDynamic(true))
+    geometry.computeBoundingSphere()
+    geometry.setDrawRange( 0, 0 )
     const material = new THREE.LineBasicMaterial({
       vertexColors: THREE.VertexColors,
       blending: this.blending === 'additive' ? THREE.AdditiveBlending : null,
       // blending: THREE.SubtractiveBlending
       transparent: true
-    });
+    })
       // blending: THREE.CustomBlending
       // blendEquation: THREE.SubtractEquation
       // blendSrc: THREE.SrcAlphaFactor
       // blendDst: THREE.OneMinusSrcAlphaFactor
 
-    this.linesMesh = new THREE.LineSegments( geometry, material );
-    this.cont.add( this.linesMesh );
+    this.linesMesh = new THREE.LineSegments( geometry, material )
+    this.cont.add( this.linesMesh )
 
     for (let i = 0; i<=n; i++) {
       for (let j = 0; j<=n; j++) {
-        const y = ri(-3, 3);
-        const x = ((i - (n/2)) * spacing) + ri(-5,5);
-        let z = ((j - (n/2)) * spacing) + ri(-5,5);
-        if (i % 2) { z += spacing * 0.5; } // offset
+        const y = ri(-3, 3)
+        const x = ((i - (n/2)) * spacing) + ri(-5,5)
+        let z = ((j - (n/2)) * spacing) + ri(-5,5)
+        if (i % 2) { z += spacing * 0.5 } // offset
 
         // nexusX = Math.round(x / 20) * 20
         // nexusZ = Math.round(z / 20) * 20
         // x += (nexusX - x) * 0.01
         // z += (nexusZ - z) * 0.01
-        this.genPoint(x, y - ri(5, 15), z);
-        this.genPoint(x + ri(-5,5), y + ri(5, 15), z + ri(-5,5));
+        this.genPoint(x, y - ri(5, 15), z)
+        this.genPoint(x + ri(-5,5), y + ri(5, 15), z + ri(-5,5))
       }
     }
 
@@ -130,9 +130,9 @@ class Effect extends VantaBase {
     this.camera = new THREE.PerspectiveCamera(
       25,
       this.width / this.height,
-      0.01, 10000);
-    this.camera.position.set(50, 100, 150);
-    this.scene.add( this.camera );
+      0.01, 10000)
+    this.camera.position.set(50, 100, 150)
+    this.scene.add( this.camera )
 
     // ambience = new THREE.AmbientLight(0xffffff, 0.01)
     // @scene.add(ambience)
@@ -141,45 +141,45 @@ class Effect extends VantaBase {
     // @pointLight.position.set(0, 150, 200)
     // @scene.add( @pointLight )
 
-    const ambience = new THREE.AmbientLight(0xffffff, 0.75);
-    this.scene.add(ambience);
+    const ambience = new THREE.AmbientLight(0xffffff, 0.75)
+    this.scene.add(ambience)
 
-    this.spot = new THREE.SpotLight(0xFFFFFF, 1);
-    this.spot.position.set(0, 200, 0);
-    this.spot.distance = 400;
-    this.spot.target = this.cont;
-    return this.scene.add(this.spot);
+    this.spot = new THREE.SpotLight(0xFFFFFF, 1)
+    this.spot.position.set(0, 200, 0)
+    this.spot.distance = 400
+    this.spot.target = this.cont
+    return this.scene.add(this.spot)
   }
 
   onUpdate() {
-    let diff, t;
+    let diff, t
     if (this.helper != null) {
-      this.helper.update();
+      this.helper.update()
     }
     if (this.controls != null) {
-      this.controls.update();
+      this.controls.update()
     }
 
-    const c = this.camera;
+    const c = this.camera
     if (Math.abs(c.tx - c.position.x) > 0.01) {
-      diff = c.tx - c.position.x;
-      c.position.x += diff * 0.02;
+      diff = c.tx - c.position.x
+      c.position.x += diff * 0.02
     }
     if (Math.abs(c.ty - c.position.y) > 0.01) {
-      diff = c.ty - c.position.y;
-      c.position.y += diff * 0.02;
+      diff = c.ty - c.position.y
+      c.position.y += diff * 0.02
     }
-    c.lookAt( new THREE.Vector3( 0, 0, 0 ) );
-    // c.near = 0.01;
-    // c.updateProjectionMatrix();
+    c.lookAt( new THREE.Vector3( 0, 0, 0 ) )
+    // c.near = 0.01
+    // c.updateProjectionMatrix()
 
-    let vertexpos = 0;
-    let colorpos = 0;
-    let numConnected = 0;
+    let vertexpos = 0
+    let colorpos = 0
+    let numConnected = 0
 
-    const bgColor = new THREE.Color(this.options.backgroundColor);
-    const color = new THREE.Color(this.options.color);
-    const diffColor = color.clone().sub(bgColor);
+    const bgColor = new THREE.Color(this.options.backgroundColor)
+    const color = new THREE.Color(this.options.color)
+    const diffColor = color.clone().sub(bgColor)
 
     if (this.rayCaster) {
       this.rayCaster.setFromCamera(new THREE.Vector2(this.rcMouseX,this.rcMouseY), this.camera);
@@ -200,69 +200,69 @@ class Effect extends VantaBase {
     // @scene.add( line )
 
     for (let i = 0; i < this.points.length; i++) {
-      var dist, distToMouse;
-      const p = this.points[i];
+      var dist, distToMouse
+      const p = this.points[i]
       // p.position.y += Math.sin(@t * 0.005 - 0.02 * p.ox + 0.015 * p.oz) * 0.02
 
       if (this.rayCaster) {
-        distToMouse = this.rayCaster.ray.distanceToPoint(p.position);
+        distToMouse = this.rayCaster.ray.distanceToPoint(p.position)
       } else {
-        distToMouse = 1000;
+        distToMouse = 1000
       }
-      const distClamp = distToMouse.clamp(5,15);
-      p.scale.x = (p.scale.y = (p.scale.z = ((15 - distClamp) * 0.25).clamp(1, 100)));
+      const distClamp = distToMouse.clamp(5,15)
+      p.scale.x = (p.scale.y = (p.scale.z = ((15 - distClamp) * 0.25).clamp(1, 100)))
 
       if (p.r !== 0) {
-        let ang = Math.atan2( p.position.z, p.position.x );
-        dist = Math.sqrt( (p.position.z * p.position.z) + (p.position.x * p.position.x) );
-        ang += 0.00025 * p.r;
-        p.position.x = dist * Math.cos(ang);
-        p.position.z = dist * Math.sin(ang);
+        let ang = Math.atan2( p.position.z, p.position.x )
+        dist = Math.sqrt( (p.position.z * p.position.z) + (p.position.x * p.position.x) )
+        ang += 0.00025 * p.r
+        p.position.x = dist * Math.cos(ang)
+        p.position.z = dist * Math.sin(ang)
       }
         // p.position.x += Math.sin(@t * 0.01 + p.position.y) * 0.02
         // p.position.z += Math.sin(@t * 0.01 - p.position.y) * 0.02
 
       for (let j = i; j < this.points.length; j++) {
-        const p2 = this.points[j];
-        const dx = p.position.x - p2.position.x;
-        const dy = p.position.y - p2.position.y;
-        const dz = p.position.z - p2.position.z;
-        dist = Math.sqrt( (dx * dx) + (dy * dy) + (dz * dz) );
+        const p2 = this.points[j]
+        const dx = p.position.x - p2.position.x
+        const dy = p.position.y - p2.position.y
+        const dz = p.position.z - p2.position.z
+        dist = Math.sqrt( (dx * dx) + (dy * dy) + (dz * dz) )
         if (dist < this.options.maxDistance) {
-          var lineColor;
-          const alpha = (( 1.0 - (dist / this.options.maxDistance) ) * 2).clamp(0, 1);
+          var lineColor
+          const alpha = (( 1.0 - (dist / this.options.maxDistance) ) * 2).clamp(0, 1)
           if (this.blending === 'additive') {
-            lineColor = new THREE.Color(0x000000).lerp(diffColor, alpha);
+            lineColor = new THREE.Color(0x000000).lerp(diffColor, alpha)
           } else {
-            lineColor = bgColor.clone().lerp(color, alpha);
+            lineColor = bgColor.clone().lerp(color, alpha)
           }
           // if @blending == 'subtractive'
           //   lineColor = new THREE.Color(0x000000).lerp(diffColor, alpha)
 
-          this.linePositions[ vertexpos++ ] = p.position.x;
-          this.linePositions[ vertexpos++ ] = p.position.y;
-          this.linePositions[ vertexpos++ ] = p.position.z;
-          this.linePositions[ vertexpos++ ] = p2.position.x;
-          this.linePositions[ vertexpos++ ] = p2.position.y;
-          this.linePositions[ vertexpos++ ] = p2.position.z;
+          this.linePositions[ vertexpos++ ] = p.position.x
+          this.linePositions[ vertexpos++ ] = p.position.y
+          this.linePositions[ vertexpos++ ] = p.position.z
+          this.linePositions[ vertexpos++ ] = p2.position.x
+          this.linePositions[ vertexpos++ ] = p2.position.y
+          this.linePositions[ vertexpos++ ] = p2.position.z
 
-          this.lineColors[ colorpos++ ] = lineColor.r;
-          this.lineColors[ colorpos++ ] = lineColor.g;
-          this.lineColors[ colorpos++ ] = lineColor.b;
-          this.lineColors[ colorpos++ ] = lineColor.r;
-          this.lineColors[ colorpos++ ] = lineColor.g;
-          this.lineColors[ colorpos++ ] = lineColor.b;
+          this.lineColors[ colorpos++ ] = lineColor.r
+          this.lineColors[ colorpos++ ] = lineColor.g
+          this.lineColors[ colorpos++ ] = lineColor.b
+          this.lineColors[ colorpos++ ] = lineColor.r
+          this.lineColors[ colorpos++ ] = lineColor.g
+          this.lineColors[ colorpos++ ] = lineColor.b
 
-          numConnected++;
+          numConnected++
         }
       }
     }
-    this.linesMesh.geometry.setDrawRange( 0, numConnected * 2 );
-    this.linesMesh.geometry.attributes.position.needsUpdate = true;
-    this.linesMesh.geometry.attributes.color.needsUpdate = true;
-    // @pointCloud.geometry.attributes.position.needsUpdate = true;
+    this.linesMesh.geometry.setDrawRange( 0, numConnected * 2 )
+    this.linesMesh.geometry.attributes.position.needsUpdate = true
+    this.linesMesh.geometry.attributes.color.needsUpdate = true
+    // @pointCloud.geometry.attributes.position.needsUpdate = true
 
-    return t = this.t * 0.001;
+    return this.t * 0.001
   }
     // @cont.rotation.x += Math.sin(t) * 0.0001
     // @cont.rotation.z += Math.cos(t) * 0.00007
