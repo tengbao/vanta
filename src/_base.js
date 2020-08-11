2c5e7d8716425de3ff04e44ac3eca8ed80357a21
Teng Bao <teng@strikingly.com>
2020-08-11 15:58:34 +0800

    upgrade three.js to r119

--- src/_base.js
+++ src/_base.js
@@ -8,7 +8,7 @@ const VANTA = (win && window.VANTA) || {}
 VANTA.register = (name, Effect) => {
   return VANTA[name] = (opts) => new Effect(opts)
 }
-VANTA.version = '0.5.17'
+VANTA.version = '0.5.18'
 
 export {VANTA}
 
